ee37c3c1ecbaeabcd128cc37acb8e8eca2bd6436
ljyw17 <46911441+ljyw17@users.noreply.github.com>
2024-03-06 13:31:31 +0800

    Add files via upload

---
+++ mmdetection/configs/dynamic_tld/dynamic_tld_r50_fpn_1x_coco.py
@@ -0,0 +1,28 @@
+_base_ = '../libra_rcnn/libra_faster_rcnn_r50_fpn_1x_coco.py'
+model = dict(
+    roi_head=dict(
+        type='DynamicRoIHeadTLD',
+        bbox_head=dict(
+            type='Shared2FCBBoxHead',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=3,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0., 0., 0., 0.],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))),
+    train_cfg=dict(
+        rpn_proposal=dict(nms=dict(iou_threshold=0.85)),
+        rcnn=dict(
+            dynamic_tld=dict(
+                iou_topk=220,
+                beta_topk=40,
+                update_iter_interval=80,
+                initial_iou=0.4,
+                initial_beta=1.0))),
+    test_cfg=dict(rpn=dict(nms=dict(iou_threshold=0.85))))
